b169c38c22af506e6c302b217cfa143c81c16f09
biillskiii <muhamad.nabil20052003@gmail.com>
2024-01-11 11:40:39 +0700

    update

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,110 +1,93 @@
-import React, { useEffect } from "react";
-import Pen9 from "../assets/Pen9.png";
-import Faq from "./Accordion";
-import Aos from "aos";
-const FaQ = () => {
-  useEffect(() => {
-    Aos.init();
-    Aos.refresh();
-  }, []);
+import React from "react";
+import {
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+} from "@material-tailwind/react";
+import { IoIosArrowDown } from "react-icons/io";
+
+const Faq = () => {
+  const [openItems, setOpenItems] = React.useState([
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
+
+  const handleOpen = (index) => {
+    const newOpenItems = openItems.map((item, i) =>
+      i === index ? !item : false
+    );
+    setOpenItems(newOpenItems);
+  };
   return (
     <>
-      <div id="faq"></div>
-      <div className="my-48">
-        <div className="w-11/12 sm:w-9/12 rounded-xl text-start mx-auto">
-          <div className="w-full flex justify-start sm:justify-center items-center">
-            <img
-              src={Pen9}
-              alt="pen9"
-              className="-mt-10 -ml-8"
-              width={100}
-              style={
-                window.matchMedia("(max-width: 640px)").matches
-                  ? { width: 40, marginRight: 28 }
-                  : {}
-              }
-            />
-            <p className="font-bold text-3xl sm:text-5xl mb-20 pt-9">
-              {" "}
-              Pertanyaan umum
-            </p>
-          </div>
-
-          <Faq
-            question={"Kenapa saya harus berlangganan?"}
-            answer={
-              "Kami memahami bahwa mungkin Anda merasa terbebani jika kami meminta Anda untuk menyiapkan sejumlah permintaan konten sekaligus untuk memenuhi jumlah paketan seperti layanan pada umumnya.Terlebih lagi, layanan seperti ini biasanya freelance, yang mungkin menimbulkan kesan kesulitan dalam melakukan transaksi berulang ketika menggunakan layanan serupa. Oleh karena itu, kami berupaya memberikan fleksibilitas permintaan konten, pembayaran, dan standar konten dengan menghadirkan fitur berlangganan jeda pada layanan kami. Tidak hanya itu, setiap paket dalam langganan ini mencakup permintaan tanpa batas, revisi tanpa batas, dan biaya berlangganan yang tetap. Selain itu, model ini dapat menjadi sumber motivasi untuk mencapai konsistensi dalam pembuatan konten, karena kita semua tahu bahwa kunci kesuksesan dalam konten adalah konsistensi."
-            }
-          />
-          <Faq
-            question={
-              "Apa bedanya jika saya langsung memesan 30 konten sekaligus secara freelance?"
-            }
-            answer={
-              "Ada beberapa perbedaan signifikan antara memesan 30 konten sekaligus secara freelance dan menggunakan layanan berlangganan kami. Pertama, biaya yang kami tawarkan adalah FLAT, yang berarti tidak akan ada biaya tambahan yang muncul secara mendadak. Selain itu, revisi juga kami sediakan tanpa biaya tambahan, memberikan fleksibilitas tanpa batas dalam mengatasi perubahan yang mungkin diperlukan. Begitu juga dengan cara berlangganan, kami memberikan nilai tambah yang berbeda. Kami beroperasi sebagai sebuah tim untuk Anda, yang bertujuan untuk menciptakan kolaborasi yang lebih efisien. Dengan menjadi bagian dari satu WhatsApp grup bersama Anda, Anda akan merasa seolah memiliki tim konten sendiri, memudahkan koordinasi dan komunikasi.Kami juga menawarkan fleksibilitas fitur jeda, sehingga Anda dapat menggunakan layanan kami sesuai kebutuhan Anda. Tak perlu khawatir untuk menyiapkan 30 konten sekaligus, berikan permintaan konten secara berkala dan jeda langganan jika tidak ada kebutuhan."
-            }
-          />
-          <Faq
-            question={"Bagaimana cara kerja satu permintaan dalam satu waktu?"}
-            answer={
-              "Sama seperti antrean streaming satu lagu di Spotify dalam satu waktu, permintaan dikerjakan satu per satu hingga selesai."
-            }
-          />
-          <Faq
-            question={"Bagaimana cara kerja fitur jeda?"}
-            answer={
-              "Kami memahami Anda mungkin tidak memiliki cukup permintaan untuk diisi sepanjang berlangganan. Mungkin saat ini Anda hanya memiliki satu atau dua permintaan konten. Di situlah menjeda langganan Anda berguna. Siklus penagihan didasarkan pada periode 31 hari. Katakanlah Anda mendaftar dan menggunakan layanan ini selama 21 hari, lalu memutuskan untuk menjeda langganan Anda. Ini berarti siklus penagihan akan dijeda dan Anda memiliki sisa layanan 10 hari untuk digunakan kapan saja di masa mendatang."
-            }
-          />
-          <Faq
-            question={"Bagaimana jika saya hanya mempunyai satu permintaan?"}
-            answer={
-              "Tidak apa-apa. Anda dapat menjeda langganan Anda setelah selesai dan kembali lagi ketika Anda memiliki kebutuhan konten lagi. Tidak perlu menyia-nyiakan sisa langganan Anda."
-            }
-          />
-          <Faq
-            question={
-              "Kenapa saya tidak memperkerjakan freelancer sendiri saja?"
-            }
-            answer={
-              "Bisa saja, tapi mengelola orang itu pusing. Proses mencarinya, hasil pengerjaannya, bahkan komunikasinya belum tentu cocok dengan Anda. Kami menghilangkan semua risiko dengan mengelola semua hal untuk Anda. Dengan paket bulanan, Anda dapat menjeda dan melanjutkan langganan sesering yang diperlukan untuk memastikan Anda hanya membayar tim konten Anda saat pekerjaan tersedia untuk mereka."
-            }
-          />
-          <Faq
-            question={"Apakah Anda yakin ini membantu saya berkembang?"}
-            answer={
-              "Setiap konten yang sukses adalah konten yang relevan bagi audiensnya. Kami tidak bisa menjamin pertumbuhan, tapi kami pastikan membantu Anda membuat konten terbaik. Kami memiliki tim konsultasi berpengalaman untuk berdiskusi dengan Anda dalam perencanaan konten Anda."
-            }
-          />
-          <Faq
-            question={
-              "Apakah ada batasan berapa banyak permintaan yang dapat saya ajukan?"
-            }
-            answer={
-              <p>
-                Setelah berlangganan, Anda dapat menambahkan permintaan ke
-                antrean sebanyak mungkin, dan permintaan tersebut akan
-                dikirimkan satu per satu (dua sekaligus jika Anda membeli paket
-                tambahan berupa "Team Boost") hingga semuanya selesai.
-              </p>
-            }
-          />
-          <Faq
-            question={"Berapa lama saya bisa menerima konten saya?"}
-            answer={
-              "Sebagian besar permintaan diselesaikan hanya dalam dua hari atau kurang. Namun, permintaan yang lebih kompleks bisa memakan waktu lebih lama."
-            }
-          />
-          <Faq
-            question={"Berapa banyak video yang saya dapatkan?"}
-            answer={
-              "Dengan rata-rata waktu penyelesaian dalam 2 hari, pelanggan biasanya menerima sekitar 16 video. Namun, jumlah ini dapat bervariasi tergantung pada tingkat kesulitan dari permintaan Anda. Jika permintaan videonya relatif mudah, maka kami dapat menyelesaikannya lebih cepat, dan Anda mungkin akan menerima lebih banyak video dalam periode yang sama."
-            }
-          />
+      <div className="my-44 max-h-[300px] flex items-start p-3  justify-center gap-20">
+        <div className="w-[492px]  flex flex-col items-start justify-start  gap-y-5">
+          <p>Frequently Asked Questions (FAQ)</p>
+          <h1 className="font-bold text-3xl">
+            Apa itu <span className="text-P2">Rakyfactory Swiftcut</span>?
+          </h1>
+          <p className="text-sm">
+            Rakyfactory Swiftcut adalah salah satu layanan Rakyfactory berupa
+            pengeditan video pendek yang menyediakan tim konten pribadi untuk
+            Anda, terdiri dari beberapa Video Editor dan Manager konten. <br />{" "}
+            <br /> Dengan berlangganan, Anda dapat mengakses permintaan konten
+            sebanyak mungkin tanpa kontrak dan biaya tambahan, sehingga Anda
+            bisa fokus mempersiapkan bahan kontennya saja atau bahkan
+            mengerjakan tugas lainnya tanpa terganggu.
+          </p>
+        </div>
+        <div className="w-[550px]">
+          {openItems.map((isOpen, index) => (
+            <div key={index} className="mb-2">
+              <Accordion
+                open={isOpen}
+                className="rounded-lg border border-blue-gray-100 px-4"
+              >
+                <AccordionHeader
+                  onClick={() => handleOpen(index)}
+                  className="border-b-0 text-sm flex justify-between items-center cursor-pointer"
+                >
+                  {index === 0 &&
+                    "Kenapa saya tidak memperkerjakan freelancer sendiri saja?"}
+                  {index === 1 && "Berapa lama saya bisa menerima konten saya?"}
+                  {index === 2 && "Bagaimana cara kerja berlangganan jeda?"}
+                  {index === 3 && "Berapa banyak video yang saya dapatkan?"}
+                  {index === 4 &&
+                    "Apa maksud 1 permintaan dan 2 permintaan dalam 1 waktu?"}
+                  <IoIosArrowDown
+                    className={`ms-auto transform ${
+                      isOpen ? "rotate-180" : "rotate-0"
+                    } transition-transform`}
+                  />
+                </AccordionHeader>
+                <div
+                  className={`${
+                    isOpen ? "block" : "hidden"
+                  } overflow-hidden transition-height`}
+                >
+                  <AccordionBody className="pt-0 text-xs font-normal">
+                    {index === 0 &&
+                      "Mengelola freelancer bisa merepotkan, dari proses rekrut hingga hasil kerja dan monitoring. Kami menghilangkan risiko itu dengan mengelola semua aspek untuk Anda. Dengan berlangganan, Anda bisa menjeda dan melanjutkan langganan sesuai kebutuhan, membayar tim pribadi hanya saat pekerjaan benar-benar ada."}
+                    {index === 1 &&
+                      "Sebagian besar permintaan diselesaikan hanya dalam 24-48 jam. Namun, permintaan yang lebih kompleks bisa memakan waktu lebih lama."}
+                    {index === 2 &&
+                      "Kami paham jika Anda hanya memiliki sedikit permintaan konten selama berlangganan. Untuk itu, kami memperkenalkan opsi jeda langganan. Dengan siklus penagihan 31 hari, jika Anda menggunakan layanan selama 21 hari dan kemudian menjeda langganan, siklus penagihan akan berhenti. Sisa 10 hari layanan dapat Anda gunakan kapan saja tanpa batas waktu kedaluwarsa."}
+                    {index === 3 &&
+                      "Setiap jenis konten akan dikategorikan berdasarkan tingkat kesulitan. Jika konten Anda termasuk kategori mudah dengan waktu pengeditan 24 jam, Anda akan menerima sekitar 31 konten selama masa langganan, jumlahnya dapat bervariasi tergantung pada tingkat kesulitan masing-masing konten."}
+                    {index === 4 &&
+                      "Setelah berlangganan, Anda bisa menambahkan sebanyak mungkin permintaan ke dalam antrean. Seperti antrean streaming lagu di Spotify, kami kerjakan 1-per-1 hingga selesai. Bergantung pada paketnya, kami juga bisa menangani 2 permintaan sekaligus, sehingga Anda mendapatkan 2 konten dalam sekali pengiriman."}
+                  </AccordionBody>
+                </div>
+              </Accordion>
+            </div>
+          ))}
         </div>
       </div>
     </>
   );
 };
 
-export default FaQ;
+export default Faq;
--- src/components/Paket.jsx
+++ src/components/Paket.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { BsArrowRight } from "react-icons/bs";
+import { PiCheckBold } from "react-icons/pi";
 
 const Paket = () => {
+  const handlePaket = () =>{
+    
+  }
   return (
     <div className="mt-44">
       <div className="flex flex-col items-center">
@@ -11,6 +16,149 @@ const Paket = () => {
         <p className="flex justify-center items-center text-sm  gap-x-1 font-medium mt-6">
           Biaya tetap. Tanpa kontrak. Tanpa pusing kelola tim.
         </p>
+      </div>
+      <div className="flex items-center justify-center gap-6 mt-10">
+        <div className="flex flex-col gap-8 items-centerjustify-center">
+          <div onClick={handlePaket} className="w-[720px] flex items-center justify-between border-2 p-5 rounded-lg">
+            <div className="w-[275px] flex flex-col items-start justify-center">
+              <p className="font-bold text-sm mb-5">Starter level</p>
+              <p className="text-3xl font-bold">
+                Rp2,199 jt{" "}
+                <span className="text-xs font-normal">/15 hari periode</span>
+              </p>
+            </div>
+            <div className="w-[275px] flex justify-end items-center">
+              <p className="text-xs font-medium text-[#5C5C5C]">
+                <span className="font-bold ">1 permintaan dalam 1 waktu.</span>{" "}
+                Cocok untuk memulai menjadi Content Creator.
+              </p>
+            </div>
+          </div>
+          <div onClick={handlePaket} className="w-[720px] flex items-center justify-between border-2 p-5 rounded-lg">
+            <div className="w-[275px] flex flex-col items-start justify-center">
+              <p className="font-bold text-sm mb-5 text-P2">Creator level</p>
+              <p className="line-through text-[#5C5C5C] text-[10px]">
+                Rp4.398.000/31 hari periode
+              </p>
+              <p className="text-3xl font-bold">
+                Rp3,199 jt{" "}
+                <span className="text-xs font-normal">/31 hari periode</span>
+              </p>
+            </div>
+            <div className="w-[275px] flex justify-end items-center">
+              <p className="text-xs font-medium text-[#5C5C5C]">
+                <span className="font-bold ">1 permintaan dalam 1 waktu.</span>{" "}
+                Cocok untuk memulai menjadi Content Creator.
+              </p>
+            </div>
+          </div>
+          <div onClick={handlePaket} className="w-[720px] flex items-center justify-between border-2 p-5 rounded-lg">
+            <div className="w-[275px] flex flex-col items-start justify-center">
+              <p className="font-bold text-sm mb-5 ">Creator PRO level</p>
+              <p className="line-through text-[#5C5C5C] text-[10px]">
+                Rp6.398.000/31 hari periode
+              </p>
+              <p className="text-3xl font-bold">
+                Rp5,499 jt{" "}
+                <span className="text-xs font-normal">/31 hari periode</span>
+              </p>
+            </div>
+            <div className="w-[275px] flex justify-end items-center">
+              <p className="text-xs font-medium text-[#5C5C5C] ">
+                <span className="font-bold ">1 permintaan dalam 1 waktu.</span>{" "}
+                Cocok untuk yang sudah aktif dalam membuat konten.
+              </p>
+            </div>
+          </div>
+          <div onClick={handlePaket} className="w-[720px] flex items-center justify-between border-2 p-5 rounded-lg">
+            <div className="w-[275px] flex flex-col items-start justify-center">
+              <p className="font-bold text-sm mb-5">Upcoming services</p>
+              <p className="flex items-center gap-2">
+                Join waitlist <BsArrowRight size={15} className="text-P2" />{" "}
+              </p>
+            </div>
+            <div className="w-[275px] flex justify-end items-center">
+              <p className="text-xs text-[#5C5C5C] font-medium">
+                Apakah Anda memerlukan solusi untuk kebutuhan lain? Silakan
+                beritahu kami.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div  className="w-[358px] h-[593px] border-2 text-start flex flex-col justify-start rounded-lg items-start px-5 py-5">
+          <h1 className="font-bold text-3xl mb-8">Included</h1>
+          <div className="flex flex-col gap-3">
+            <p className="flex items-center justify-start gap-x-3 text-lg">
+              <div className="rounded-full flex justify-center items-center bg-P1 w-7 h-7">
+                <PiCheckBold className="text-P2" size={15} />
+              </div>
+              Bonus pemilihan klip YT
+            </p>
+            <p className="flex items-center justify-start gap-x-3 text-lg">
+              <div className="rounded-full flex justify-center items-center bg-P1 w-7 h-7">
+                <PiCheckBold className="text-P2" size={15} />
+              </div>
+              Bonus visual-content style
+            </p>
+            <p className="flex items-center justify-start gap-x-3 text-lg">
+              <div className="rounded-full flex justify-center items-center bg-P1 w-7 h-7">
+                <PiCheckBold className="text-P2" size={15} />
+              </div>
+              Unlimited request konten
+            </p>
+            <p className="flex items-center justify-start gap-x-3 text-lg">
+              <div className="rounded-full flex justify-center items-center bg-P1 w-7 h-7">
+                <PiCheckBold className="text-P2" size={15} />
+              </div>
+              Gratis unlimited revisi
+            </p>
+            <p className="flex items-center justify-start gap-x-3 text-lg">
+              <div className="rounded-full flex justify-center items-center bg-P1 w-7 h-7">
+                <PiCheckBold className="text-P2" size={15} />
+              </div>
+              Gratis cloud storage
+            </p>
+            <p className="flex items-center justify-start gap-x-3 text-lg">
+              <div className="rounded-full flex justify-center items-center bg-P1 w-7 h-7">
+                <PiCheckBold className="text-P2" size={15} />
+              </div>
+              Gratis konsultasi konten
+            </p>
+            <p className="flex items-center justify-start gap-x-3 text-lg">
+              <div className="rounded-full flex justify-center items-center bg-P1 w-7 h-7">
+                <PiCheckBold className="text-P2" size={15} />
+              </div>
+              Obrolan tim sefrekuensi
+            </p>
+            <p className="flex items-center justify-start gap-x-3 text-lg">
+              <div className="rounded-full flex justify-center items-center bg-P1 w-7 h-7">
+                <PiCheckBold className="text-P2" size={15} />
+              </div>
+              Tim support 24/7
+            </p>
+            <p className="flex items-center justify-start gap-x-3 text-lg">
+              <div className="rounded-full flex justify-center items-center bg-P1 w-7 h-7">
+                <PiCheckBold className="text-P2" size={15} />
+              </div>
+              Langganan terjeda otomatis
+            </p>
+            <p className="flex items-center justify-start gap-x-3 text-lg">
+              <div className="rounded-full flex justify-center items-center bg-P1 w-7 h-7">
+                <PiCheckBold className="text-P2" size={15} />
+              </div>
+              Tanpa biaya tambahan
+            </p>
+            <p className="flex items-center justify-start gap-x-3 text-lg">
+              <div className="rounded-full flex justify-center items-center bg-P1 w-7 h-7">
+                <PiCheckBold className="text-P2" size={15} />
+              </div>
+              Tanpa kontrak
+            </p>
+          </div>
+          <a className="text-P2 mt-6 text-base flex items-center gap-2">
+            Daftar sekarang <BsArrowRight size={15} className="text-P2" />
+          </a>
+        </div>
       </div>
     </div>
   );
