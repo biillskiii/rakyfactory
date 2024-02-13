aa036ec66fadca7ad5ca2ddb005b6adb387b6e9c
biillskiii <muhamad.nabil20052003@gmail.com>
2024-02-13 22:20:08 +0700

    update

--- src/components/MobilePopupModal.jsx
+++ src/components/MobilePopupModal.jsx
@@ -5,10 +5,12 @@ const MobilePopupModal = () => {
   return (
     <div className="modal fixed top-0 left-0 right-0 bottom-0 w-full h-full flex items-center justify-center bg-white z-50">
       <div className="bg-white w-96 flex flex-col gap-y-5 justify-center items-center p-10 rounded-md shadow-md">
-        <MdOutlineDevices
-          size={200}
-          className="max-w-full h-auto sm:max-w-xs md:max-w-md lg:max-w-lg xl:max-w-xl"
-        />
+        <div className="bg-[#F9F9F9] rounded-[15px] px-16 py-10">
+          <MdOutlineDevices
+            size={200}
+            className="max-w-full h-auto sm:max-w-xs md:max-w-md lg:max-w-lg xl:max-w-xl"
+          />
+        </div>
 
         <p className="text-center text-xs lg:text-sm flex flex-col items-center justify-center">
           <span className="font-bold text-xl my-2 lg:text-2xl ">
